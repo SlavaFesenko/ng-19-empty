391eb5644e562d3c9b1ed769de6bb26bee626fdf
Slava Fesenko <fvsleader@gmail.com>
2024-12-17 10:22:51 +0200

    routing changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterModule, RouterOutlet],
+  template: `
+    <a routerLinkActive="active" routerLink="">Example</a> |
+    <a routerLinkActive="active" routerLink="/other-404">Not created yet</a> |
+
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   title = 'ng-19-empty';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './components/example/example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ExampleComponent },
+  { path: 'example', component: ExampleComponent },
+  { path: '**', redirectTo: 'example' },
+];
